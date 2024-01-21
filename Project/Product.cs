1be34e12abe0cbef977f0596a311888681bdeec0
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2024-01-21 23:37:04 +0200

    Creating project, adding class Product

---
+++ Project/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Project
+{
+    internal abstract class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Count { get; private set; }
+
+        public abstract string Display();
+    }
+}
