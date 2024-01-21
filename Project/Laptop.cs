2e44bd9823e4c01e5f8cd526c435bacbe27faadd
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2024-01-22 00:27:30 +0200

    Adding class Laptop, modifying Product

---
+++ Project/Laptop.cs
@@ -0,0 +1,24 @@
+﻿namespace Project
+{
+    internal class Laptop : Product
+    {
+        public string Producer { get; protected set; }
+
+        public Laptop() : base()
+        {
+            Producer = "producer";
+        }
+
+        public Laptop(string producer, string name, double price, int count) : base(name, price, count)
+        {
+            if (producer == null) throw new ArgumentNullException();
+            if (producer.Length == 0) throw new ArgumentException("producer name cannot be empty");
+            Producer = producer;
+        }
+
+        public override string Display()
+        {
+            return $"{Producer}, {Name}, {Price}, {Count}";
+        }
+    }
+}
--- Project/Product.cs
+++ Project/Product.cs
@@ -2,9 +2,27 @@
 {
     internal abstract class Product
     {
-        public string Name { get; private set; }
-        public double Price { get; private set; }
-        public int Count { get; private set; }
+        public string Name { get; protected set; }
+        public double Price { get; protected set; }
+        public int Count { get; protected set; }
+
+        public Product()
+        {
+            Name = "product";
+            Price = 0;
+            Count = 0;
+        }
+
+        protected Product(string name, double price, int count)
+        {
+            if (name == null) throw new ArgumentNullException();
+            if (name.Length == 0) throw new ArgumentException("name cannot be empty");
+            Name = name;
+            if (price < 0) throw new ArgumentOutOfRangeException(nameof(price));
+            Price = price;
+            if (count < 0) throw new ArgumentOutOfRangeException(nameof(count));
+            Count = count;
+        }
 
         public abstract string Display();
     }
