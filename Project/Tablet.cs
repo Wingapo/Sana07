89164654ebe49b6bad3b4f301999ee78a1e30f2d
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2024-01-22 00:37:48 +0200

    Adding class Tablet

---
+++ Project/Tablet.cs
@@ -0,0 +1,23 @@
+﻿namespace Project
+{
+    internal class Tablet : Product
+    {
+        public double Diagonal { get; protected set; }
+
+        public Tablet() : base() 
+        {
+            Diagonal = 0;
+        }
+
+        public Tablet(string name, double diagonal, double price, int count) : base(name, price, count)
+        {
+            if (diagonal < 0) throw new ArgumentOutOfRangeException(nameof(diagonal));
+            Diagonal = diagonal;
+        }
+
+        public override string Display()
+        {
+            return $"{Name}, {Diagonal}, {Price}, {Count}";
+        }
+    }
+}
